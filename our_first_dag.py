8445c3c1f21b06229bf0d88f0ea6685a1e5cf455
borabora1495-spec <borabora1495@gmail.com>
2026-05-17 12:09:21 +0700

    add load expand to preview

--- our_first_dag.py
+++ our_first_dag.py
@@ -125,6 +125,6 @@ def daily_etl_pipeline():
     raw_files = extract_market_data.expand(market=markets)
     transformed_files = transform_market_data.expand(raw_file=raw_files)
     load_to_mysql.expand(transformed_file=transformed_files)
-
+    load_to_mysql.expand(transformed_file=transformed_files) >> preview_mysql
 dag = daily_etl_pipeline()
 
